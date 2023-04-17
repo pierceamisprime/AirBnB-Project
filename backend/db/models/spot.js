f1951a312eb07d1f16e9f1992cbdd99dbab83ec3
Pierce <henriksbopierce@gmail.com>
2023-04-17 13:41:50 -0700

    fixed spot/user association

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Spot.belongsTo(models.Users, {
+      Spot.belongsTo(models.User, {
         foreignKey: 'ownerId'
       })
     }
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Spots, {
+      User.hasMany(models.Spot, {
         foreignKey: 'ownerId'
       })
     }
