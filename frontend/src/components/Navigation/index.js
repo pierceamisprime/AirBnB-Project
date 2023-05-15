c9cb17d8b3b494dcd8ea8a8083520cc9b4869915
Pierce <henriksbopierce@gmail.com>
2023-05-14 18:42:12 -0700

    css

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -10,7 +10,7 @@ function Navigation({ isLoaded }) {
     return (
         <ul className='nav'>
             <li className='home'>
-                <NavLink className='link-home' exact to="/"><i class="fa-solid fa-dumbbell fa-2xl" style={{color: "#FF5A5F",}} />   fitbnb</NavLink>
+                <NavLink className='link-home' exact to="/"><i class="fa-solid fa-dumbbell fa-2xl" style={{color: "#FF5A5F",}} />   FitBnB</NavLink>
             </li>
             <li className='create-spot'>
             {sessionUser ?  <NavLink className= 'link-new' to='/spots/new'>Create a New Spot</NavLink> : null}
